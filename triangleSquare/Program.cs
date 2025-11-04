4cde1d0dba983ab1c9ab246f5d72c14220b2cfc1
rdsm <rdsm@03-1K-404-1>
2025-11-04 10:54:46 +0400

    Добавьте файлы проекта.

---
+++ triangleSquare/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace triangleSquare
+{
+    class squareOfTriangle
+    {
+        static void Main(string[] args)
+        {
+            double S = 0, p = 0, a = 0;
+
+            Console.WriteLine("Enter the perimeter of a regular triangle");
+            p = Double.Parse(Console.ReadLine()) / 2;
+            a = 2* p / 3;
+            S = Math.Sqrt(p * (p - a) * (p - a) * (p - a));
+            Console.WriteLine("Side   Square");
+            Console.WriteLine("{0}   {1}", a, S);
+        }
+    }
+}
